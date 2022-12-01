04538801998edaf2f3bdfeb4bda12bcc47a074aa
hyobb <hyobin1009@gmail.com>
2022-12-02 01:58:06 +0900

    sorting words

--- unsolved/1181.c
+++ unsolved/1181.c
@@ -1,32 +1,41 @@
 // 단어 정렬
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-typedef struct
+int compare(const void *x, const void *y) // x와 y는 strs의 원소를 가리키는 포인터
 {
-    char *word[51];
-    int wlen;
-}list;
+	// strs가 char **이므로 맞춰서 형변환하고 값 역참조
+	char *s1 = *(char **)x;
+    char *s2 = *(char **)y;
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+    if (len1 != len2) // s1이 s2보다 길면 양수 반환, 짧으면 음수 반환
+		return (len1 - len2);
+    return (strcmp(s1, s2)); // 길이가 같으면 알파벳 순으로 정렬
+}
 
-list copy[]
-void merge(list )
 int main()
 {
     int n = 0;
-
     scanf("%d", &n);
-    char words[n][51];
+    char **strs = (char **)malloc(sizeof(char *)*(n + 1));
     for (int i = 0; i < n; i++)
     {
-        scanf("%s", words[i]);
+        strs[i] = (char *)malloc(sizeof(char) * 51);
+        scanf("%s", strs[i]);
     }
-    printf("----answer----\n");
-    qsort(words, n, sizeof(words), compare);
+    strs[n] = 0;
+    // printf("=====result=====\n");
+    qsort(strs, n, sizeof(strs[0]), compare);
     for (int i = 0; i < n - 1; i++)
     {
-
+		if (strcmp(strs[i], strs[i + 1]))
+			printf("%s\n", strs[i]);
+        free(strs[i]);
     }
-    //for (int i = 0; i < n; i ++)
-        //printf("%s\n", words[i]);
+	printf("%s\n", strs[n - 1]);
+	free(strs[n - 1]);
+    free(strs);
+    return 0;
 }
