af9eef70edf1fee5ca2df5bed309d9606d604fa8
hyobb <hyobin1009@gmail.com>
2022-12-07 00:07:54 +0900

    solved

--- silver/1914.js
+++ silver/1914.js
@@ -2,19 +2,22 @@
 let input = require('fs').readFileSync('test.txt').toString().trim();
 //let input = require('fs').readFileSync('/dev/stdin').toString().trim();
 
-function towerOfHanoi(a, b, n) {
+function towerOfHanoi(a, b, n, res) {
 	if (n === 1) {
-		console.log(a, b);
+		res.push(`${a} ${b}`);
 		return;
 	}
-	towerOfHanoi(a, 6 - a - b, n - 1);
-	console.log(a, b);
-	towerOfHanoi(6 - a - b, b, n - 1);
+	towerOfHanoi(a, 6 - a - b, n - 1, res);
+	res.push(`${a} ${b}`);
+	towerOfHanoi(6 - a - b, b, n - 1, res);
 }
 
+let res = [];
 let n = Number(input);
-let n_big = BigInt(n);
-let k = 2n ** n_big - 1n;
+let bigN = BigInt(n);
+let k = 2n ** bigN - 1n;
 console.log(String(k));
-if (n <= 20)
-	towerOfHanoi(1, 3, n);
+if (n <= 20) {
+	towerOfHanoi(1, 3, n, res);
+	console.log(res.join('\n'));
+}
