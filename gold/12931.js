bb471a625e6884180b38c6cdbfc20fbf6bf3de0a
hyobin <hyobb109@gmail.com>
2023-06-04 22:13:41 +0900

    23/06/04 두 배 더하기

--- gold/12931.js
+++ gold/12931.js
@@ -1,9 +1,9 @@
 // 두 배 더하기
 let [n, input] = require('fs').readFileSync('test.txt').toString().trim().split('\n');
-let a = new Array(+n);
 let b = input.split(' ').map(Number);
 let cnt = 0;
-while (b.reduce((acc, el) => acc + el, 0)) {
+let total = b.reduce((acc, el) => acc + el, 0);
+while (total !== 0) {
  	b.forEach((el, i) => {
 		if (el % 2) {
 			b[i]--;
@@ -11,7 +11,8 @@ while (b.reduce((acc, el) => acc + el, 0)) {
 		}
 	});
 	b.forEach((el, i) => b[i] = el / 2);
-	if (!b.reduce((acc, el) => acc + el, 0)) break;
+	total = b.reduce((acc, el) => acc + el, 0);
+	if (total === 0) break;
 	cnt++;
 }
 console.log(cnt);
