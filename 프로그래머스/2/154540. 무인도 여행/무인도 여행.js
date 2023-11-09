579f496d877fed6644b8f406bf92a0b25a748d1c
hyobb109 <105159293+hyobb109@users.noreply.github.com>
2023-11-09 16:04:01 +0900

    [level 2] Title: 무인도 여행, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -3,25 +3,25 @@ function solution(maps) {
     const dy = [-1, 1, 0, 0];
     const col_len = maps.length;
     const row_len = maps[0].length;
-    let visited = new Array(col_len).fill(false).map(() => new Array(row_len).fill(false));
     let res = [];
     let q = [];
+    let map = maps.map(el => el.split(""));
     for (let i = 0; i < col_len; ++i) {
         for (let j = 0; j < row_len; ++j) {
-            if (visited[i][j] || maps[i][j] === 'X') continue;
+            if (map[i][j] === 'X') continue;
             q.push([i, j]);
-            visited[i][j] = true;
-            let area = +maps[i][j];
+            let area = +map[i][j];
+            map[i][j] = 'X';
             while (q.length) {
                 let [x, y] = q.shift();
                 for (let dir = 0; dir < 4; ++dir) {
                     let cx = x + dx[dir];
                     let cy = y + dy[dir];
                     if (cx < 0 || cy < 0 || cx >= col_len || cy >= row_len) continue;
-                    if (visited[cx][cy] || maps[cx][cy] === 'X') continue;
+                    if (map[cx][cy] === 'X') continue;
                     q.push([cx, cy]);
-                    area += +maps[cx][cy];
-                    visited[cx][cy] = true;
+                    area += +map[cx][cy];
+                    map[cx][cy] = 'X';
                 }
             }
             res.push(area);
