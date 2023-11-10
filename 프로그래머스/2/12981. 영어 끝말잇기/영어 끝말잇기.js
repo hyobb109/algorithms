1bbf85e77aa0a1b5944eff340883fbcf382e0a5a
hyobb109 <105159293+hyobb109@users.noreply.github.com>
2023-11-10 14:26:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -7,8 +7,8 @@ function solution(n, words) {
         let order = parseInt(i / n) + 1;
         if (i && words[i - 1].at(-1) !== words[i].at(0))
         return [person, order];
+        if (set.has(words[i])) return [person, order];
         set.add(words[i]);
-        if (set.size !== i + 1) return [person, order];
     }
     
     return [0, 0];
