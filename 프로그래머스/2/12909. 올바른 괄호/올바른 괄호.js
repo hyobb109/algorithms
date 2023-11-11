4cd5d06d941ef99c59885886c41d725d8f659e17
hyobb109 <105159293+hyobb109@users.noreply.github.com>
2023-11-11 23:45:48 +0900

    [level 2] Title: 올바른 괄호, Time: 2.64 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,12 @@
+function solution(s){
+    let open = 0;
+    let len = s.length
+    for (let i = 0; i < len; ++i) {
+        if (s[i] == '(') open += 1;
+        else {
+            if (open < 1) return false;
+            open -= 1;
+        }
+    }
+    return open > 0 ? false : true;
+}
