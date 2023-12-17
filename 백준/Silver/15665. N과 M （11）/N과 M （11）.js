74f7b571d2392eca1d2d4edfc5da9560ba053a02
hyobb109 <105159293+hyobb109@users.noreply.github.com>
2023-12-17 14:50:51 +0900

    [Silver II] Title: N과 M (11), Time: 468 ms, Memory: 64548 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.js
@@ -0,0 +1,32 @@
+let input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const numbers = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+let res = '';
+let comb = new Array(M);
+
+function getComb(n) {
+  if (n === M) {
+    res += comb.join(' ') + '\n';
+    return;
+  }
+  let tmp = 0;
+  for (let i = 0; i < N; i++) {
+    const k = numbers[i];
+    if (tmp === k) continue;
+    comb[n] = k;
+    tmp = k;
+    getComb(n + 1);
+  }
+}
+
+getComb(0);
+console.log(res);
