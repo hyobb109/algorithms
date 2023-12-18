bf3248674078a54c889b970af11a35d6b0542a44
hyobb109 <105159293+hyobb109@users.noreply.github.com>
2023-12-18 23:41:49 +0900

    [level 2] Title: 튜플, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let combs = s.slice(2, s.length-2).split('},{');
+    combs.sort((a, b) =>  a.length > b.length ? 1 : -1);
+    let tuple = combs.map(el => el.split(','));
+    let res = [];
+    let set = new Set();
+    tuple.forEach(comb => {
+        comb.forEach((num) => {
+            if (!set.has(num)) {
+                set.add(num);
+                res.push(+num);
+            }
+        })
+    })
+    return res;
+}
