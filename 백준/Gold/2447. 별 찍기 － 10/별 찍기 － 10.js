44a4843a4f7f82438a91c95d3356938ceec363a4
hyobb109 <105159293+hyobb109@users.noreply.github.com>
2023-12-21 03:50:08 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 488 ms, Memory: 98560 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.js
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.js
@@ -1,19 +1,20 @@
 const N = +require('fs').readFileSync('/dev/stdin').toString().trim();
-let res = '';
+let res = new Array(N).fill(0).map(() => new Array(N).fill(' '));
 
 function drawStar(n, x, y) {
-  if (parseInt(x / n) % 3 === 1 && parseInt(y / n) % 3 === 1) res += ' ';
-  else {
-    n === 1 ? (res += '*') : drawStar(n / 3, x, y);
+  if (n === 1) {
+    res[x][y] = '*';
+    return;
   }
-}
-
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    drawStar(N, i, j);
+  for (let i = 0; i < 3; i++) {
+    const cx = x + (n / 3) * i;
+    for (let j = 0; j < 3; j++) {
+      const cy = y + (n / 3) * j;
+      if (i === 1 && j === 1) continue;
+      else drawStar(n / 3, cx, cy);
+    }
   }
-  res += '\n';
 }
 
 drawStar(N, 0, 0);
-console.log(res.slice(0, -1));
+console.log(res.map((el) => el.join('')).join('\n'));
