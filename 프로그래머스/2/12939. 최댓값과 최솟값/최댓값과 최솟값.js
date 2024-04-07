0825992b063fea89072adf093250771fd08e6028
hyobicho <105159293+hyobb109@users.noreply.github.com>
2024-04-08 00:00:52 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,6 +1,4 @@
 function solution(s) {
     let arr = s.split(' ').map(Number).sort((a, b) => a - b);
-    console.log(arr);
-    let res = [arr[0], arr[arr.length - 1]].join(' ');
-    return res;
+    return [arr[0], arr[arr.length - 1]].join(' ');
 }
