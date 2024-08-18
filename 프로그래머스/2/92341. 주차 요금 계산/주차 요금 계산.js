ae9d3edf71e775b827bf072607ab2cdede838313
hyobicho <105159293+hyobb109@users.noreply.github.com>
2024-08-18 15:48:13 +0900

    [level 2] Title: 주차 요금 계산, Time: 14.41 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -0,0 +1,38 @@
+function solution(fees, records) {
+    let res = [];    
+    const getTimeDiff = (inTime, outTime) => {
+        const inTimeToDate = new Date(`2024/08/15 ${inTime}:00`);
+        const outTimeToDate = new Date(`2024/08/15 ${outTime}:00`);
+        return (outTimeToDate - inTimeToDate) / (60 * 1000);
+    }
+    
+    const getTotalFee = (time) => {
+        let total = fees[1];
+        const extraTime = time - fees[0];
+        if (extraTime > 0) {
+            total += Math.ceil(extraTime / fees[2]) * fees[3];
+        }
+        return total;
+    }
+    
+    const paymentMap = new Map();
+    records.map((record) => {
+        const [time, car, log] = record.split(' ');
+        const carNum = Number(car);
+        const val = paymentMap.get(carNum);
+        if (log === 'OUT') {            
+            const parkingTime = val.totalMin + getTimeDiff(val.inTime, time);
+            paymentMap.set(carNum, {inTime: null, totalMin: parkingTime});
+        } else {
+            val ? paymentMap.set(carNum, {inTime: time, totalMin: val.totalMin}) :
+            paymentMap.set(carNum, {inTime: time, totalMin: 0});
+        }
+    });
+    console.log(paymentMap);
+    for (const [carNum, data] of paymentMap) {
+        const {inTime, totalMin} = data;
+        const totalFee = inTime ? getTotalFee(totalMin + getTimeDiff(inTime, '23:59')) : getTotalFee(totalMin);
+        res.push([carNum, totalFee]);
+    }
+    return res.sort((a, b) => a[0] - b[0]).map(el => el[1]);
+}
