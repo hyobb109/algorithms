c9ab024c3b5ab7d6b8dd54995f61f675884e856d
hyobicho <105159293+hyobb109@users.noreply.github.com>
2024-10-30 20:43:52 +0900

    [Gold IV] Title: 문자열 폭발, Time: 280 ms, Memory: 78648 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.js
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.js
@@ -9,9 +9,15 @@ const bombLen = bomb.length;
 const strLen = str.length;
 for (let i = 0; i < strLen; i++) {
   stack.push(str[i]);
-  if (str[i] === bomb[bombLen - 1]) {
-    let cmp = stack.slice(-bombLen);
-    if (cmp.join('') === bomb) stack.splice(-bombLen);
+  let tmp = stack.length - 1;
+  let bombIdx = bombLen - 1;
+  while (bombIdx >= 0) {
+    if (stack[tmp] !== bomb[bombIdx]) break;
+    tmp -= 1;
+    bombIdx -= 1;
+  }
+  if (bombIdx === -1) {
+    for (let j = 0; j < bombLen; j++) stack.pop();
   }
 }
 
